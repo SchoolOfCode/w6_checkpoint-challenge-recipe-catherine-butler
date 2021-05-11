c0aacb9490265619dd5af4c8122bda8016a0eff6
Catherine Butler <catbutler039@gmail.com>
2021-05-11 10:35:31 +0100

    pseudo-code plan and attempt at handleFoodChange function without google

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ function handleRecipeClick() {
 }
 
 function handleFoodChange() {
-  foodToSearch = document.querySelector("#food-input").value;
+  // this funciton needs, to turn the user input into a variable 
+  // called "food" to be read by "fetch recipe", does this need to be a readable string  
+
+  foodToSearch = document.querySelector("#food-input").value = text;
+  let foodToSearch = food.value;
+  return String(food)
 }
 
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  //--- write your code above ---
+  // this function needs to fetch info from the api which contains the keyword held by "food"
+  // and return it as a display in innerHTML
+  const response = await fetch ('https://api.edamam.com/search');
+  const recipes = await response.json();
+  console.log(recipes);
 }
+
+fetchRecipe()
